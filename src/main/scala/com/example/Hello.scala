2f03cb15849c4eca924e28cd3950583c06659f21
Ankur Sharma <Ankur Sharma>
2016-10-23 17:33:27 +0100

    sdsd

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -5,9 +5,7 @@ object Hello {
     println("Oh Hello, world!")
   }
 
-<<<<<<< HEAD
-  def hi = println("hi")
-=======
-  def home = println("time to go home") 
->>>>>>> 2c793fd715d4f33fb28c99e491fac53d6a269f95
+  def home = println("time to go home")
+
+  def hi = println("hi...")
 }
